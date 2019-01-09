4428459b84e288216c7ab0529e91f6f79f619d4d
Justin Hallier <justin.hallier@ezyvet.com>
2019-01-09 20:36:48 +1300

    Added login view and some logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,9 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "antd";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {LoginView} from './Components/Views/LoginView';
+import { LoginContainer } from "./Components/Views/LoginContainer";
 
-const login = () => <div>This is the login page</div>;
 const index = () => <div>This is the index page</div>;
 
 class App extends Component {
@@ -25,7 +26,7 @@ class App extends Component {
           </div>
 
         <Route path="/" exact={true} component={index} />
-        <Route path="/login" component={login} />
+        <Route path="/login" component={LoginContainer} />
         </div>
       </Router>
     );
