bfcc1635ee4fa98968cb51eaf4dc6d5722d30410
Justin Hallier <justin.hallier@ezyvet.com>
2019-01-11 13:18:53 +1300

    Add documentation

--- frontend/src/lib/fetch.ts
+++ frontend/src/lib/fetch.ts
@@ -1,3 +1,9 @@
+/**
+ * Make a request to the backend. Send with a bearer token if one has been received.
+ * @param url URL to send the request to
+ * @param method Type of request. Should be one of GET, POST or PATCH
+ * @param data If posting, send this as the body
+ */
 export async function makeRequest(url: string, method = "GET", data = {}) {
   const token = localStorage.getItem("token");
   const fetchOptions: RequestInit = {
