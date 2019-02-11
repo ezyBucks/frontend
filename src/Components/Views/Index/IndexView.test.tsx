a869e193c7f148c0779aa5bbb44c33cae0fc516f
Justin Hallier <justin.hallier@ezyvet.com>
2019-02-11 13:31:41 +1300

    Added enzyme for testing
    Also updated tests to use enzyme.

--- src/Components/Views/Index/IndexView.test.tsx
+++ src/Components/Views/Index/IndexView.test.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 import renderer from 'react-test-renderer';
 import { IndexView } from './IndexView';
+import { shallow, mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
 
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDom.render(<IndexView />, div);
+    shallow(<IndexView />);
 });
 
-// Snapshot testing
+it('fully renders without crashing', () => {
+    const wrapper = mount(
+        <MemoryRouter>
+            <IndexView />
+        </MemoryRouter>
+    );
+    expect(wrapper.contains(<IndexView />)).toBe(true);
+});
+
 it('matches the snapshot', () => {
-    const index = renderer
-    .create(<IndexView />)
-    .toJSON();
+    const index = renderer.create(<IndexView />).toJSON();
     expect(index).toMatchSnapshot();
 });
-
-// Also check if we can use enzyme for these
