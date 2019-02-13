065e48b1cb20bdea8faad549b9f8f5de018e39e1
Justin Hallier <justin.hallier@ezyvet.com>
2019-02-13 16:49:08 +1300

    Update urls to point to aws instance

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 const API_ADDRESS = {
-    URL: 'http://localhost',
+    URL: process.env.REACT_APP_SERVER_HOST ? 'http://ec2-13-239-33-36.ap-southeast-2.compute.amazonaws.com' : 'http://localhost',
     PORT: '8081'
 };
 
