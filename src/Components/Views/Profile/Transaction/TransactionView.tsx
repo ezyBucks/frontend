fee7eca9af70b2549b11349e3b2eeaa2939507fd
jallier <justin.hallier+git@gmail.com>
2019-02-27 16:57:54 +1300

    Added amount field

--- src/Components/Views/Profile/Transaction/TransactionView.tsx
+++ src/Components/Views/Profile/Transaction/TransactionView.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { ContainerDiv } from '../../../Common/Styles';
 import AutoComplete, { DataSourceItemType } from 'antd/lib/auto-complete';
-import { Select, Spin } from 'antd';
+import { Select, Spin, InputNumber, Button } from 'antd';
 import { User } from '../../../../types/User';
 import { UserSelect } from './TransactionContainer';
 
@@ -22,11 +22,11 @@ const TransactionView: React.SFC<TransactionViewProps> = props => {
                 marginTop: '30px'
             }}
         >
-            <div style={{width: '400px', paddingBottom:'25px'}}>
+            <div style={{ width: '400px', paddingBottom: '15px' }}>
                 <h3>Transfer</h3>
                 <Select
                     notFoundContent={props.fetching && <Spin size={'small'} />}
-                    placeholder={'Select users to transfer'}
+                    placeholder={'Select users to transfer to'}
                     mode={'multiple'}
                     style={{ width: '70%' }}
                     onSearch={props.fetchUsers}
@@ -38,8 +38,24 @@ const TransactionView: React.SFC<TransactionViewProps> = props => {
                     ))}
                 </Select>
             </div>
+            <div style={{paddingBottom: '15px'}}>
+                <h3>Amount of ezyBucks to send</h3>
+                <InputNumber
+                    defaultValue={1}
+                    formatter={value =>
+                        `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                    }
+                    parser={value =>
+                        parseInt(
+                            value ? value.replace(/\$\s?|(,*)/g, '') : '',
+                            10
+                        )
+                    }
+                    min={1}
+                />
+            </div>
             <div>
-                <h3>Amount of ezybucks to send</h3>
+                <Button>Send!</Button>
             </div>
         </ContainerDiv>
     );
