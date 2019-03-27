481f43c2200228716be2dcc8066d1a5ba2a871e4
Justin Hallier <justin.hallier@ezyvet.com>
2019-03-27 15:33:26 +1300

    Fix unit tests

--- src/Components/Views/Index/IndexView.test.tsx
+++ src/Components/Views/Index/IndexView.test.tsx
@@ -3,21 +3,38 @@ import renderer from 'react-test-renderer';
 import { IndexView } from './IndexView';
 import { shallow, mount } from 'enzyme';
 import { MemoryRouter } from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import MainReducer from '../../../store/rootReducer';
+
+// Create a store to use in the test
+const store = createStore(MainReducer);
 
 it('renders without crashing', () => {
     shallow(<IndexView />);
 });
 
+/**
+ * This may not be possible due to the redux containers
+ */
 it('fully renders without crashing', () => {
     const wrapper = mount(
-        <MemoryRouter>
-            <IndexView />
-        </MemoryRouter>
+        <Provider store={store}>
+            <MemoryRouter>
+                <IndexView />
+            </MemoryRouter>
+        </Provider>
     );
     expect(wrapper.contains(<IndexView />)).toBe(true);
 });
 
 it('matches the snapshot', () => {
-    const index = renderer.create(<IndexView />).toJSON();
+    const index = renderer
+        .create(
+            <Provider store={store}>
+                <IndexView />
+            </Provider>
+        )
+        .toJSON();
     expect(index).toMatchSnapshot();
 });
