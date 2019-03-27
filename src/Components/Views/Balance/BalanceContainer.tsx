3e804c63edbd8cabe67e424d23c57fec4aa52823
Justin Hallier <justin.hallier@ezyvet.com>
2019-03-27 13:45:26 +1300

    Formatted with prettier

--- src/Components/Views/Balance/BalanceContainer.tsx
+++ src/Components/Views/Balance/BalanceContainer.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as Types from '../../../types/Types';
-import {Skeleton} from "antd";
-import {BalanceView} from "./BalanceView";
+import { Skeleton } from 'antd';
+import { BalanceView } from './BalanceView';
 
 interface BalanceContainerProps {
     userId: number;
@@ -11,8 +11,10 @@ interface BalanceContainerState {
     balance: Types.Balance | undefined;
 }
 
-export class BalanceContainer extends React.Component<BalanceContainerProps, BalanceContainerState> {
-
+export class BalanceContainer extends React.Component<
+    BalanceContainerProps,
+    BalanceContainerState
+> {
     constructor(props: BalanceContainerProps) {
         super(props);
 
@@ -32,9 +34,9 @@ export class BalanceContainer extends React.Component<BalanceContainerProps, Bal
 
     public render() {
         if (this.state.balance === undefined) {
-            return <Skeleton active={true} />
+            return <Skeleton active={true} />;
         }
 
-        return <BalanceView balance={this.state.balance.balance} />
+        return <BalanceView balance={this.state.balance.balance} />;
     }
 }
